7b859f1bfa87a4381ed90b2318de5fa2883c2c8c
Darpan Chapagain <chapagaindarpan11@gmail.com>
2022-04-27 01:10:43 +0545

    homepage update

--- resources/js/components/pages/Home.vue
+++ resources/js/components/pages/Home.vue
@@ -29,37 +29,8 @@
           >
 
           <v-card-actions>
-            <v-btn text> Find Talent </v-btn>
+            <v-btn text @click="register"> Find Talent </v-btn>
           </v-card-actions>
-        </v-card>
-      </v-container>
-    </div>
-    <div class="m-4">
-      <v-container>
-        <v-card color="#952175" dark>
-          <div class="d-flex flex-no-wrap justify-space-between">
-            <div>
-              <v-card-title class="text-h5"
-                >WorkOnLancing Saved My life</v-card-title
-              >
-
-              <v-card-text class="text-h5 font-weight-bold">
-                "Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo, aliquid. Voluptatibus soluta alias harum incidunt nihil! Facilis, numquam? Similique doloremque molestiae officia vero totam saepe illo atque, eos repellat quidem, beatae eveniet ipsa consequuntur fuga qui quasi tempora modi reprehenderit suscipit voluptatum aut nisi quas incidunt quaerat? Et, explicabo quo.."
-              </v-card-text>
-
-              <v-card-actions>
-                <v-btn class="ml-2 mt-5" outlined rounded small>
-                  See Profile!
-                </v-btn>
-              </v-card-actions>
-            </div>
-
-            <v-avatar class="ma-3" size="125" tile>
-              <v-img
-                src="https://cdn.vuetifyjs.com/images/cards/halcyon.png"
-              ></v-img>
-            </v-avatar>
-          </div>
         </v-card>
       </v-container>
     </div>
@@ -69,6 +40,11 @@
 <script>
 export default {
   name: "HomeHero",
+  methods: {
+    register() {
+      this.$router.push("/register");
+    },
+  },
 };
 </script>
 
