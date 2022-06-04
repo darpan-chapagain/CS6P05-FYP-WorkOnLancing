fc62a1c49ed1be4e947dd5c83e6a86579d7ba25c
Darpan Chapagain <chapagaindarpan11@gmail.com>
2022-06-04 14:02:39 +0545

    seedbackus

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,8 +16,8 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
         $this->call(RoleSeeder::class);
         $this->call(SkillSeeder::class);
-        $this->call(TestUserSeeder::class);
         $this->call(JobCategorySeeder::class);
+        $this->call(TestUserSeeder::class);
         // $this->call(ChatRoomSeeder::class);
 
     }
